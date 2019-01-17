c34a0c042112173bd6e0c668fdd8892673a2d1fd
jimilee <46700478+jimilee@users.noreply.github.com>
2019-01-17 15:51:14 +0900

    수정

--- train.py
+++ train.py
@@ -1,2 +1,1 @@
 num_class = 3
-
--- traindata_get.py
+++ traindata_get.py
@@ -1,8 +1,9 @@
 from konlpy.tag import Kkma
-from konlpy.utils import pprint
-import requests
+from urllib.request import urlopen
+#import requests
 from bs4 import BeautifulSoup
 kkma = Kkma()
+getUrl = '{}'
 def Data_Preprocessing():
     file = open('data.txt','r')
     file_result = open('data_preprocessed.txt','w')
@@ -11,22 +12,19 @@ def Data_Preprocessing():
         nouns = kkma.nouns(line.split(',')[0])
         print(nouns)
         if(nouns != []):
-            file_result.writelines(nouns +list(',')+list(line.split(',')[1]))
-            file_result.write('\n')
+            file_result.writelines(nouns +list(','+line.split(',')[1]))
+            #file_result.write('\n')
 def getData(url):
 
+    webpage = urlopen(url)
+    soup = BeautifulSoup(webpage, 'html.parser')
     file = open('data.txt','a')
-
-    req = requests.get(url)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-
     sentence = soup.select('div.reporter_line > dl > dd')
     score = soup.select('div.re_score_grp > div > div > em')
 
     for score_value, value in zip(score, sentence):
         print(value.text.strip(), score_value.text.strip())
-        file.writelines(value.text.strip().replace(',',' ') +','+ score_value.text.strip() + '\n')
+        file.writelines(value.text.strip().replace(',',' ') +',\''+ score_value.text.strip() + '\'\n')
 
 
     small_sc = soup.select('li > div.star_score > em')
@@ -34,7 +32,7 @@ def getData(url):
 
     for score_value, value in zip(small_sc, small_se):
         print(value.text.strip(), score_value.text.strip())
-        file.writelines(value.text.strip().replace(',',' ') +','+ score_value.text.strip()+ '\n')
+        file.writelines(value.text.strip().replace(',',' ') +',\''+ score_value.text.strip()+ '\'\n')
 
     file.close()
 
